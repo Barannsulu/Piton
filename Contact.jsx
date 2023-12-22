b3e0cd96fb002b6805c000a79bd90b0bc938c2c2
Baran Sülü <73203587+Barannsulu@users.noreply.github.com>
2023-12-22 05:30:33 +0300

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Link from "next/link";
+function Contact() {
+  return (
+    <div className="sm:flex p-12">
+      <div className="bg p-4 w-1/2 h-auto flex justify-center items-center mr-20 hidden sm:flex">
+        <img src="/images/contact.png" alt="" />
+      </div>
+      <div className="sm:w-1/2 p-4 flex flex-col relative text-center">
+        <h2 className="text-3xl font-bold mb-8 text-right">
+          We'd love to hear <br /> <p className="text-right">from you</p>
+        </h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            id="input1"
+            name="input1"
+            className="mt-1 p-4 border x w-full  placeholder-black"
+            placeholder="Name*"
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="email"
+            id="input2"
+            name="input2"
+            className="mt-1 p-4 border  w-full placeholder-black"
+            placeholder="Email*"
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="text"
+            id="input3"
+            name="input3"
+            className="mt-1 p-4 border w-full placeholder-black"
+            placeholder="Website URL*"
+          />
+        </div>
+
+        <div className="mb-4">
+          <textarea
+            type="text"
+            id="input4"
+            name="input4"
+            className="mt-1 p-4 border w-full h-[200px] resize-none placeholder-black"
+            placeholder="Project Details"
+          />
+        </div>
+
+        <div className="mb-4">
+          <Link
+            href="https://www.youtube.com/watch?v=h0MJZyK5m5w&t=78s"
+            type="submit"
+            className="bg-black text-white p-2 w-full "
+          >
+            Send Proposal
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
